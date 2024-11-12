bda19797a8de39b9dda661bc7344bb7fa02b265f
Ganesha Ram G <125204007+ganesha62@users.noreply.github.com>
2024-11-12 16:39:36 +0530

    Update Config.js

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'https://guardmind-backend.onrender.com';
+const API_BASE_URL = 'https://guardmind-backend-1.onrender.com';
    export const apiClient = axios.create({
      baseURL: API_BASE_URL,
    });
